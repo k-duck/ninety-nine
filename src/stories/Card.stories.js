4b4ce457b01828bf042866c15d4e013376b9ba97
Brad Duck <brad.w.duck@gmail.com>
2020-06-03 17:04:17 -0400

    Added knobs to the Card story, updated eslint to not give import errors

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { withKnobs, select } from "@storybook/addon-knobs";
 import styled from "styled-components";
 import Card from "../card/Card";
 import { SUIT } from "../card/constants";
 
 export default {
   title: "Card",
-  component: Card
+  component: Card,
+  decorators: [withKnobs]
 };
 
 const SuitContainer = styled.div`
@@ -13,8 +15,16 @@ const SuitContainer = styled.div`
   height: 300px;
 `;
 
+const suitOptions = [SUIT.spades, SUIT.hearts, SUIT.diamonds, SUIT.clubs];
+const suitLabel = "Suit";
+const rankOptions = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const rankLabel = "Rank";
+
 export const CardIcon = () => (
-    <SuitContainer>
-      <Card suit={SUIT.spades} rank={"A"}/>
-    </SuitContainer>
+  <SuitContainer>
+    <Card
+      suit={select(suitLabel, suitOptions, SUIT.spades)}
+      rank={select(rankLabel, rankOptions, "A")}
+    />
+  </SuitContainer>
 );
