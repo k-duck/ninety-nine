3bedcb20ba9d546928c52bf30f3608c68980495c
Brad Duck <brad.w.duck@gmail.com>
2020-06-04 09:21:30 -0400

    Added width knob

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withKnobs, select } from "@storybook/addon-knobs";
+import { withKnobs, select, number } from "@storybook/addon-knobs";
 import styled from "styled-components";
 import Card from "../card/Card";
 import { SUIT } from "../card/constants";
@@ -10,21 +10,34 @@ export default {
   decorators: [withKnobs]
 };
 
-const SuitContainer = styled.div`
-  width: 200px;
-  height: 300px;
-`;
-
 const suitOptions = [SUIT.spades, SUIT.hearts, SUIT.diamonds, SUIT.clubs];
 const suitLabel = "Suit";
 const rankOptions = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const rankLabel = "Rank";
+const numberOptions = {
+  range: true,
+  min: 50,
+  max: 500,
+  step: 25
+};
+const widthLabel = "Width in Pixels";
 
-export const CardIcon = () => (
-  <SuitContainer>
-    <Card
-      suit={select(suitLabel, suitOptions, SUIT.spades)}
-      rank={select(rankLabel, rankOptions, "A")}
-    />
-  </SuitContainer>
-);
+const SuitContainer = styled.div`
+  width: ${props => props.width};
+  height: ${props => props.height};
+`;
+
+export const CardIcon = () => {
+  const width = number(widthLabel, 200, numberOptions);
+
+  console.log({ width });
+
+  return (
+    <SuitContainer width={`${width}px`} height={`${(width * 300) / 200}px`}>
+      <Card
+        suit={select(suitLabel, suitOptions, SUIT.spades)}
+        rank={select(rankLabel, rankOptions, "A")}
+      />
+    </SuitContainer>
+  );
+};
