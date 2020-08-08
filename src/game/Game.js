0627ade926c78ebf8b10c166ee4c764324e6d133
Brad Duck <brad.w.duck@gmail.com>
2020-08-08 17:42:28 -0400

    Removed Table component

--- src/game/Game.js
+++ src/game/Game.js
@@ -2,8 +2,10 @@ import React from "react";
 import produce from "immer";
 import styled from "styled-components";
 import ScaleText from "react-scale-text";
-import Table from "../table/Table";
+import AspectRatioBox from "../card/AspectRatioBox";
+import Card from "../card/Card";
 import CardDeck from "../cardDeck/CardDeck";
+import Player from "../player/Player";
 import { GameContext } from "./GameContext";
 
 const testMode = true;
@@ -40,6 +42,123 @@ const initialPlayers = [
 ];
 
 const deck = new CardDeck();
+
+const seatingChart = [
+  [0, 6],
+  [0, 4, 8],
+  [0, 3, 6, 9],
+  [0, 2, 5, 7, 10],
+  [0, 2, 4, 6, 8, 10],
+  [0, 1, 3, 5, 7, 9, 11],
+  [0, 2, 3, 4, 6, 8, 9, 10],
+  [0, 2, 3, 4, 5, 7, 8, 9, 10],
+  [0, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+];
+
+const seatingPositions = [
+  { x: "50%", y: "100%" },
+  { x: "27%", y: "90%" },
+  { x: "10%", y: "80%" },
+  { x: "3%", y: "50%" },
+  { x: "10%", y: "20%" },
+  { x: "30%", y: "5%" },
+  { x: "50%", y: "0%" },
+  { x: "70%", y: "5%" },
+  { x: "90%", y: "20%" },
+  { x: "97%", y: "50%" },
+  { x: "90%", y: "80%" },
+  { x: "73%", y: "90%" }
+];
+
+const TableShape = styled.div`
+  position: relative;
+  height: 100%;
+  box-sizing: border-box;
+  border-radius: 50%;
+  background-color: green;
+`;
+
+const TableContainer = styled.div`
+  padding: 3%;
+  width: 70vw;
+`;
+
+const PlayerContainer = styled.div`
+  width: ${props => (props.user ? "25%" : "14%")};
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: ${props => seatingPositions[props.seat].x};
+  top: ${props => seatingPositions[props.seat].y};
+`;
+
+const TableCenter = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+  width: 50%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const DiscardPile = styled.div`
+  width: 20%;
+`;
+
+const ScoreContainer = styled.div`
+  width: 10%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const GameContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HandOverToast = styled.div`
+  position: absolute;
+  background-color: white;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+  z-index: 2;
+  width: 30%;
+  height: 15%;
+  padding: 2%;
+  border: 2px solid black;
+`;
+
+const LosingText = styled.span`
+  color: red;
+  width: 100%;
+  height: 100%;
+  white-space: nowrap;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Score = styled.span`
+  color: ${props => (props.currentScore > 89 ? "red" : "black")};
+  -webkit-text-stroke: 1px
+    ${props => (props.currentScore > 89 ? "black" : "none")};
+  -webkit-text-fill-color: ${props =>
+    props.currentScore > 89 ? "red" : "black"};
+`;
 
 const Game = () => {
   const [players, setPlayers] = React.useState(initialPlayers);
@@ -141,36 +260,6 @@ const Game = () => {
     setPlayers(newPlayers);
   }, []);
 
-  const GameContainer = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const HandOverToast = styled.div`
-    position: absolute;
-    background-color: white;
-    transform: translate(-50%, -50%);
-    left: 50%;
-    top: 50%;
-    z-index: 2;
-    width: 30%;
-    height: 15%;
-    padding: 2%;
-    border: 2px solid black;
-  `;
-
-  const LosingText = styled.span`
-    color: red;
-    width: 100%;
-    height: 100%;
-    white-space: nowrap;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
   return (
     <GameContext.Provider value={{ playCard, canCardBePlayed }}>
       <GameContainer>
@@ -181,11 +270,43 @@ const Game = () => {
             </ScaleText>
           </HandOverToast>
         )}
-        <Table
-          players={players}
-          lastCardPlayed={lastCardPlayed}
-          currentScore={currentScore}
-        ></Table>
+        <TableContainer>
+          <AspectRatioBox ratio={0.5}>
+            <TableShape>
+              {players.map(({ user, cards, active, coins, name }, idx) => (
+                <PlayerContainer
+                  user={user}
+                  seat={seatingChart[players.length - 2][idx]}
+                  key={name}
+                >
+                  <Player
+                    cards={cards}
+                    user={user}
+                    active={active}
+                    coins={coins}
+                    name={name}
+                  />
+                </PlayerContainer>
+              ))}
+              <TableCenter>
+                <ScoreContainer>
+                  <ScaleText>
+                    <Score currentScore={currentScore}>{currentScore}</Score>
+                  </ScaleText>
+                </ScoreContainer>
+                {lastCardPlayed && (
+                  <DiscardPile>
+                    <Card
+                      rank={lastCardPlayed.rank}
+                      suit={lastCardPlayed.suit}
+                      faceUp
+                    />
+                  </DiscardPile>
+                )}
+              </TableCenter>
+            </TableShape>
+          </AspectRatioBox>
+        </TableContainer>
       </GameContainer>
     </GameContext.Provider>
   );
