349608d1d25d50787f65cd097a814dd5031f5182
Kylie Duck <kyliemduck@gmail.com>
2020-09-06 15:30:15 -0400

    Updates

---
+++ precache-manifest.affeeeef6a7e40531cbf8a6d3a68d198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313a91efeeb19f775dba1d364647e800",
+    "url": "/ninety-nine/index.html"
+  },
+  {
+    "revision": "f96d37eaba10c18f5c90",
+    "url": "/ninety-nine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b54029a83821390338c",
+    "url": "/ninety-nine/static/js/2.1b6093dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ninety-nine/static/js/2.1b6093dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96d37eaba10c18f5c90",
+    "url": "/ninety-nine/static/js/main.58ff0786.chunk.js"
+  },
+  {
+    "revision": "79e7e4d14d2728bcaad4",
+    "url": "/ninety-nine/static/js/runtime-main.524118d7.js"
+  }
+]);
